a32b451f98973c229d30491ad129d3f321a254e1
caiya <caiya928@aliyun.com>
2018-02-13 11:55:06 +0800

    修改主页

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -62,14 +62,26 @@
       </el-menu>
     </el-aside>
       <el-main>
-        <el-table :data="tableData">
-          <el-table-column prop="date" label="日期" width="140">
-          </el-table-column>
-          <el-table-column prop="name" label="姓名" width="120">
-          </el-table-column>
-          <el-table-column prop="address" label="地址">
-          </el-table-column>
-        </el-table>
+        <el-container>
+          <el-header style="">
+              <el-breadcrumb separator="/">
+                <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+                <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+                <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+                <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+              </el-breadcrumb>
+          </el-header>
+          <el-main>
+            <el-table :data="tableData">
+              <el-table-column prop="date" label="日期" width="140">
+              </el-table-column>
+              <el-table-column prop="name" label="姓名" width="120">
+              </el-table-column>
+              <el-table-column prop="address" label="地址">
+              </el-table-column>
+            </el-table>
+          </el-main>
+        </el-container>
       </el-main>
     </el-container>
     <el-container>
@@ -94,7 +106,7 @@
 
 <script>
 export default {
-  name: 'Main',
+  name: "Main",
   data() {
     const item = {
       date: "2016-05-02",
